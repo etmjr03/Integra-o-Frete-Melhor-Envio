6b796526dde5044f586a16703ec75eebcd39d624
Edilson Junior <etmjr03@gmail.com>
2024-08-21 23:15:55 -0300

    First commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//PAINEL
+Route::prefix('produto')->group(function(){
+    Route::get('/listagem', function(){
+        return view('produto.listagem.produto-listagem');
+    })->name('listagem');
+
+    Route::get('detalhe', function(){
+        return view('produto.detalhe.produto-detalhe');
+    })->name('detalheProduto');
+});
