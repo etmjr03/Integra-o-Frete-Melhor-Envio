b46e516bc97ff7dfbe8a49b80638fb3d75f503f7
Edilson Junior <etmjr03@gmail.com>
2024-08-21 23:39:41 -0300

    Adicionando model e migration para a integração

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.default')
 
 @section('conteudo')
-  <h1>Detalhe do produto</h1>
+  <h1>Logar com Melhor Envio</h1>
 @endsection
--- resources/views/produto/listagem/produto-listagem.blade.php
+++ resources/views/produto/listagem/produto-listagem.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  @extends('layouts.default')
+@extends('layouts.default')
 
-  @section('conteudo')
-    <h1>Listagem de produto</h1>
-  @endsection
-</body>
-</html>
+@section('conteudo')
+  <h1>Listagem de produto</h1>
+@endsection
