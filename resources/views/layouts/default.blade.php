887dd2666da8bee2004e17ffa18ba9eb286437d6
Edilson Junior <etmjr03@gmail.com>
2024-08-22 22:55:44 -0300

    Ajustando layouts

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,15 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/resources/css/usuario.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <title>Frete - Melhor Envio</title>
+  <title>@yield('titlePage', 'Frete - Melhor Envio')</title>
 </head>
-<body>
-  <div class="">Nav</div>
-  <div class="container">
+<body class="bg-dark">
+  <div class="container d-flex justify-content-center text-center">
     @yield('conteudo')
   </div>
-  <div class="">Footer</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/usuario/usuario.blade.php
+++ resources/views/usuario/usuario.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.default')
 
+@section('titlePage', 'Informações de usuário')
+
+@extends('layouts.nav')
+
 @section('conteudo')
-  <h1>Informações de Usuário - Melhor Envio</h1>
-  
-  <img src="{{ $informacoesUsuario->picture }}" alt="">
-  <p>Nome: {{ $informacoesUsuario->firstname }}</p>
-  <p>Sobrenome: {{ $informacoesUsuario->lastname }}</p>
-  <p>E-mail: {{ $informacoesUsuario->email }}</p>
-  <p>limite de cotação de fretes: {{ $informacoesUsuario->limits->shipments }}</p>
+  <div class="mt-3 text-light">
+    <h4>Informações de Usuário - Melhor Envio</h4>
+    
+    <img src="{{ $informacoesUsuario->picture }}" alt="" style="width: 200px; height: 200px; border-radius: 50%;">
+    <p>Nome: {{ $informacoesUsuario->firstname }} {{ $informacoesUsuario->lastname }}</p>
+    <p>E-mail: {{ $informacoesUsuario->email }}</p>
+    <p>limite de cotação de fretes: {{ $informacoesUsuario->limits->shipments }}</p>
+  </div>
 @endsection
+
+@extends('layouts.footer')
